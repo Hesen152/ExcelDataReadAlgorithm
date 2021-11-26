50523baabb97d472074157e473a306f31f08e9de
Hesen Huseynov <katalizator1522@gmail.com>
2021-11-27 03:04:51 +0400

    tsk

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel; 
+
+namespace ExcelDataReadd
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Excel.Application xlApp ;
+            Excel.Workbook xlWorkBook ;
+            Excel.Worksheet xlWorkSheet ;
+            Excel.Range range ;
+
+            string str;
+            int rCnt ;
+            int cCnt ;
+            int rw = 0;
+            int cl = 0;
+
+            xlApp = new Excel.Application();
+            xlWorkBook = xlApp.Workbooks.Open(@"C:\Users\User\Videos\sozler.xlsx", 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+            
+            range = xlWorkSheet.UsedRange;
+            rw = range.Rows.Count;
+            cl = range.Columns.Count;
+            
+            for (rCnt = 1; rCnt  <= rw; rCnt++)
+            {
+                List<string> arraywords = new();
+
+                for (cCnt = 1; cCnt <= cl; cCnt++)
+                {
+                    str = (string)(range.Cells[rCnt, cCnt] as Excel.Range).Value2;
+                    Console.WriteLine(str);
+                              arraywords.Add(str); 
+            
+                }
+
+            
+
+
+            }
+            
+
+            xlWorkBook.Close(true, null, null);
+            xlApp.Quit();
+
+            Marshal.ReleaseComObject(xlWorkSheet);
+            Marshal.ReleaseComObject(xlWorkBook);
+            Marshal.ReleaseComObject(xlApp);
+            
+
+        }
+    }
+}
